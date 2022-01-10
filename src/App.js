7e344c96266218a509bd26e3a59e691f23d7cffc
Luke3199 <b.luke3199@gmail.com>
2022-01-10 15:38:30 +0100

    Fix App.js ADM branch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom'
 import AccountProvider from './providers/accountProvider'
+import {toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import InformazioniPersonaliUtente from './pages/informazioniPersonaliUtente/informazioniPersonaliUtente'
 import AreaPersonaleUtente from './pages/areaPersonaleUtente/areaPersonaleUtente'
 import HomePage from './pages/HomePage/homepage'
-import SignUp from './pages/Signup/signup'
+import SignUp from './pages/SignUp/signUp'
 import ConnectedLogin from './pages/Login/login'
 import ConnectedDoctorList from './pages/doctorList/doctorList'
-import {toast} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import Recovery from './components/recovery/Recovery';
 import ConnectedRecover from './pages/recovery/recover'; 
 import SocialLink from './components/social_link/Sociallink';
+import ModificaPassword from './pages/modificaPassword/modificaPassword';
+import SignUp2 from './pages/SignUp/signUp2';
+import CancellaAccount from './pages/cancellaAccount/cancellaAccount';
 
 import './App.css';
-import ModificaPassword from './pages/modificaPassword/modificaPassword';
-import SignUp2 from './pages/Signup/signUp2';
-import CancellaAccount from './pages/cancellaAccount/cancellaAccount';
+
 
 require('dotenv').config({ path: '../.env' })
 
@@ -28,12 +29,12 @@ function App() {
 
       <AccountProvider>
         <Switch>
-          <Route exact path="/cancellaAccount" component={CancellaAccount}/>
+          <Route exact path="/deleteAccount" component={CancellaAccount}/>
           <Route exact path="/secondRegister" component={SignUp2}/>
-          <Route exact path="/" component={HomePage}/>
-          <Route exact path="/modificaPassword" component={ModificaPassword}/>
-          <Route exact path="/informazioniPersonaliUtente" component={InformazioniPersonaliUtente}/>
-          <Route exact path="/areaPersonaleUtente" component={AreaPersonaleUtente}/>
+          <Route exact path="/home" component={HomePage}/>
+          <Route exact path="/editPassword" component={ModificaPassword}/>
+          <Route exact path="/userDetails" component={InformazioniPersonaliUtente}/>
+          <Route exact path="/userArea" component={AreaPersonaleUtente}/>
           <Route exact path="/signup" component={SignUp}/>
           <Route exact path="/login" component={ConnectedLogin}/>
           <Route exact path="/search/:location/:dept" component={ConnectedDoctorList}/>
