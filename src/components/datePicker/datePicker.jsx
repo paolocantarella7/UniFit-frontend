61711b64822352c858a62ad174ccb3ef0fda382a
Francesco <kekkopacio@gmail.com>
2022-01-11 16:59:59 +0100

    Aggiunto componente MultipleDatePicker

---
+++ src/components/datePicker/datePicker.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react"
+import DatePicker from "react-multi-date-picker"
+
+export default function MultipleDatePicker() {
+  const today = new Date()
+  const tomorrow = new Date()
+
+  tomorrow.setDate(tomorrow.getDate() + 1)
+
+  const [values, setValues] = useState([today, tomorrow])
+
+  return (
+    <DatePicker 
+      multiple
+      value={values} 
+      onChange={setValues}
+    />
+  )
+}
