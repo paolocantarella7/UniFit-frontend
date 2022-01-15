a7324ac29471c98525eefd5e446b3cf00b047364
Luke3199 <b.luke3199@gmail.com>
2022-01-15 15:59:37 +0100

    [ADM] fix modal cancella struttura

--- src/pages/visualizzaStrutture/visualizzaStrutture.jsx
+++ src/pages/visualizzaStrutture/visualizzaStrutture.jsx
@@ -7,8 +7,9 @@ import Server from "../../config.json";
 
 class VisualizzaStrutture extends React.Component {
   state = {
-    structures:[],
-    loading: true
+    structures: [],
+    loading: true,
+    idToDelete: -1,
   }
 
   componentDidMount() {
@@ -16,7 +17,7 @@ class VisualizzaStrutture extends React.Component {
   }
 
   struttureGet() {
-    var url = Server.API_URL+"admin/strutture/visualizzastrutture"
+    var url = Server.API_URL + "admin/strutture/visualizzastrutture"
     fetch(url)
       .then(response => response.json())
       .then(responseJson => {
@@ -28,65 +29,79 @@ class VisualizzaStrutture extends React.Component {
   }
 
 
-  handleDelete = strutturaId => {
-      this.setState({ loading: true })
-      var url = Server.API_URL+`admin/strutture/eliminastruttura?idStrutt=${strutturaId}`
-      console.log(url)
-      fetch(url)
-        .then(response => response.json())
-        .then(responseJson => {
-            if(responseJson.code === '200')
-            {
-              this.struttureGet(); 
-            }
-              window.location.reload(false);
-        })
-        .catch(error => console.log(error))
+  handleDelete = () => {
+    this.setState({ loading: true })
+    var url = Server.API_URL + `admin/strutture/eliminastruttura?idStrutt=${this.state.idToDelete}`
+    console.log(url)
+    fetch(url)
+      .then(response => response.json())
+      .then(responseJson => {
+        if (responseJson.code === '200') {
+          this.struttureGet();
+        }
+        window.location.reload(false);
+      })
+      .catch(error => console.log(error))
   }
 
-    render() {
-        return (
-          this.state.loading ?
-          <>
+  render() {
+    return (
+      this.state.loading ?
+        <>
           <ConnectedHeader
             {...this.props}
-           currentUser={new User("admin", "Luigi")}
-            type= "admin"/>
-          
+            currentUser={new User("admin", "Luigi")}
+            type="admin" />
+
           <div className="container-fluid text-dark rounded w-75 text-center bg-white my-4">
             <h1 className="pt-4">Caricamento Strutture</h1>
           </div>
-          
+
           <Footer {...this.props} />
-          </>
-          :
-          <>
-            <ConnectedHeader
-              {...this.props}
-              currentUser={new User("admin", "Luigi")}
-              type= "admin"/>
-            
-            <div className="container-fluid text-dark rounded w-75 text-center bg-white my-4">
-              <h1 className="pt-4">Visualizza strutture</h1>
+        </>
+        :
+        <>
+          <ConnectedHeader
+            {...this.props}
+            currentUser={new User("admin", "Luigi")}
+            type="admin" />
 
-              <div className='col'>
-                {(this.state.structures.length === 0) ? (
-                  <p>Non ci sono strutture!</p>
-                ):(
-                  this.state.structures.map(struttura =>(
+          <div className="container-fluid text-dark rounded w-75 text-center bg-white my-4">
+            <h1 className="pt-4">Visualizza strutture</h1>
+
+            <div className='col'>
+              {(this.state.structures.length === 0) ? (
+                <p>Non ci sono strutture!</p>
+              ) : (
+                this.state.structures.map(struttura => (
                   <CardStruttura
-                    key = {struttura.id}
-                    onDelete = {this.handleDelete}
-                    struttura = {struttura} />   
-                  )))
-                }
-              </div>
-
+                    key={struttura.id}
+                    onPress={(id) => this.setState({ idToDelete: id })}
+                    struttura={struttura} />
+                )))
+              }
             </div>
-            <Footer {...this.props} />
-            </>
-        );
-      }
+
+          </div>
+          <Footer {...this.props} />
+
+          <div className="modal" id="modalElimina" tabIndex="-1" role="dialog">
+            <div className="modal-dialog" role="document">
+              <div className="modal-content">
+                <div className="modal-body">
+                  <p>Sicuro di cancellare la struttura??</p>
+                </div>
+                <div className="modal-footer">
+                  <button type="button" className="btn btn-danger" data-dismiss="modal" onClick={() => this.handleDelete()}>Cancella</button>
+                  <button type="button" className="btn btn-secondary" data-dismiss="modal">Annulla</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </>
+    );
+  }
 }
 
 export default VisualizzaStrutture;
