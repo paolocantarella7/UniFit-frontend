f243fb29d6f2112325fe6066d0045b847477ae87
GGFlutterdev <luigi.allocca12345@live.it>
2022-01-16 18:10:44 +0100

    [PRU] Implementazione login e registrazione e head

--- src/App.js
+++ src/App.js
@@ -1,51 +1,39 @@
-import React from 'react';
-import {Switch, Route} from 'react-router-dom'
-import AccountProvider from './providers/accountProvider'
-import {toast} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+//import pagine Utente
+import Login from "./pages/Login/login";
+import SignUp from "./pages/SignUp/signup";
+import SignUp2 from "./pages/SignUp/signUp2";
+import Recovery from "./components/recovery/Recovery";
+import ConnectedRecover from "./pages/recovery/recover";
+import HomePage from "./pages/HomePage/homepage";
+import AreaPersonaleUtente from "./pages/areaPersonaleUtente/areaPersonaleUtente";
+import InformazioniPersonaliUtente from "./pages/informazioniPersonaliUtente/informazioniPersonaliUtente";
+import ModificaPassword from "./pages/modificaPassword/modificaPassword";
+import CancellaAccount from "./pages/cancellaAccount/cancellaAccount";
 
-import InformazioniPersonaliUtente from './pages/informazioniPersonaliUtente/informazioniPersonaliUtente'
-import AreaPersonaleUtente from './pages/areaPersonaleUtente/areaPersonaleUtente'
-import HomePage from './pages/HomePage/homepage'
-import SignUp from './pages/SignUp/signUp'
-import ConnectedLogin from './pages/Login/login'
-import ConnectedDoctorList from './pages/doctorList/doctorList'
-import Recovery from './components/recovery/Recovery';
-import ConnectedRecover from './pages/recovery/recover'; 
-import SocialLink from './components/social_link/Sociallink';
-import ModificaPassword from './pages/modificaPassword/modificaPassword';
-import SignUp2 from './pages/SignUp/signUp2';
-import CancellaAccount from './pages/cancellaAccount/cancellaAccount';
+import "./App.css";
 
-import './App.css';
-
-
-require('dotenv').config({ path: '../.env' })
-
-toast.configure()
+require("dotenv").config({ path: "../.env" });
+toast.configure();
 function App() {
   return (
     <div className="App">
-
-      <AccountProvider>
-        <Switch>
-          <Route exact path="/deleteAccount" component={CancellaAccount}/>
-          <Route exact path="/secondRegister" component={SignUp2}/>
-          <Route exact path="/home" component={HomePage}/>
-          <Route exact path="/editPassword" component={ModificaPassword}/>
-          <Route exact path="/userDetails" component={InformazioniPersonaliUtente}/>
-          <Route exact path="/userArea" component={AreaPersonaleUtente}/>
-          <Route exact path="/signup" component={SignUp}/>
-          <Route exact path="/login" component={ConnectedLogin}/>
-          <Route exact path="/search/:location/:dept" component={ConnectedDoctorList}/>
-          <Route exact path="/recovery/:token" component={Recovery}/>
-          <Route exact path="/recover" component={ConnectedRecover}/>
-          <Route  path="/redirect/:url"  component={SocialLink} />
-
-        </Switch>
-      </AccountProvider>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/signup" component={SignUp} />
+        <Route exact path="/secondRegister" component={SignUp2} />
+        <Route exact path="/recover" component={ConnectedRecover} />
+        <Route exact path="/recovery/:token" component={Recovery} />
+        <Route exact path="/home" component={HomePage} />
+        <Route exact path="/userArea" component={AreaPersonaleUtente} />
+        <Route exact path="/userDetails" component={InformazioniPersonaliUtente} />
+        <Route exact path="/editPassword" component={ModificaPassword} />
+        <Route exact path="/deleteAccount" component={CancellaAccount} />
+      </Switch>
     </div>
   );
 }
-
 export default App;
