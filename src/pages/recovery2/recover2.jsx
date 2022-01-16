123fc5ae3de75c384f364529bed4d6c5d6c7dda6
GGFlutterdev <luigi.allocca12345@live.it>
2022-01-17 00:47:06 +0100

    [PRU] recupero password 2

--- src/pages/recovery2/recover2.jsx
+++ src/pages/recovery2/recover2.jsx
@@ -1,140 +1,149 @@
-import React from 'react';
-import ConnectedHeader from '../../components/header/header';
-import Footer from '../../components/footer/footer'
-import { toast } from 'react-toastify';
-import Loading from '../../components/loading/loading';
+import React from "react";
+import ConnectedHeader from "../../components/header/header";
+import Footer from "../../components/footer/footer";
+import { Redirect, Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import Loading from "../../components/loading/loading";
+import Server from "../../config.json";
 
-
-// Recovery page
+// Login page
 class Recover2 extends React.Component {
   constructor() {
     super();
     this.state = {
-      currentUser: false,
-      email: "",
-      pass: "",
+      password: "",
+      confermaPassword: "",
       errors: {},
-      loading:false
-    }
+      loading: false,
+      user: {},
+    };
   }
   handleChange = (event) => {
     event.preventDefault();
     const { name, value } = event.target;
-    this.setState({ [name]: value })
-  }
+    this.setState({ [name]: value });
+  };
   formSubmit = (e) => {
     e.preventDefault();
     let errors = {};
-    if (!this.state.email)
-      errors.email = "Please enter Email";
-
-    if (errors.email) {
+    if (!this.state.password)
+      errors.password = "Inserire la nuova password";
+    if (!this.state.confermaPassword)
+      errors.confermaPassword = "Inserire la conferma della password"
+    if (errors.password || errors.confermaPassword) {
       this.setState({ errors })
       return;
     } else {
-      let data = {
-        email: this.state.email,
-      }
-      this.setState({loading:true})
-      fetch(`${process.env.REACT_APP_SERVER_DOMAIN}/reset/`, {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-      }).then(response => {
-        if (!response.ok) {
-          throw new Error(response.status);
-        } else
-        toast.success('Password reset link is sent by email, Please check your mail.', {
-          autoClose: 3000,
-          className: "successToast"
-        })
-        setTimeout(function () {
-          window.location.href = '/login';
-      }, 3000);
-        
-      }
-      // ).then(user => {
-      //   this.setState({loading:false})
-      //   if (user.role === 'doctor' && user.is_verified) {
-      //     console.log(user)
-      //   } else {
-          // toast.error('Your account is not verified.', {
-          //   autoClose: 3000,
-          //   className: "errorToast"
-          // })
-      //   }
-        
-      // }
-      ).catch(error => {
-        this.setState({loading:false})
-        let errors = {
-          invalid: "User not found"
-        }
-        toast.error('User not found', {
-          autoClose: 3000,
-          className: "errorToast"
-        })
-        this.setState({ errors })
-      })
+      var data = new FormData()
+      let user = localStorage.getItem('currentUser')
+      user = JSON.parse(user);
+      data.append('password', this.state.password)
+      data.append('passwordConferma', this.state.confermaPassword)
     }
+    this.setState({ loading: true })
+
+    var url = Server.API_URL + "reset-password?token="+this.props.match.params.token
+    fetch(url, {
+      method: 'POST',
+      body: data
+    }).then(response =>  response.json()
+    ).then(responseJson => {
+      
+      this.setState({ loading: false })
+      console.log(responseJson)
+      if (responseJson.codice === 200){    
+        localStorage.removeItem("currentUser");
+        localStorage.setItem("isLogged", false);
+        window.location.assign(Server.FRONT_URL);
+      }
+    })
   }
 
   render() {
+    if (localStorage.getItem("isLogged") === "true") {
+      let user = localStorage.getItem("currentUser");
+      user = JSON.parse(user);
+
+      if (user.isAdmin) {
+        return <Redirect to="/adminArea" />;
+      } else {
+        return <Redirect to="/home" />;
+      }
+    } else {
       return (
         <div className="pb-4">
-          <ConnectedHeader {...this.props} />
-          <div className="signup_main ">
-            <div className="signup_right mx-auto pb-5 my-5">
-              <div className="right_top">
-                <h1 className="heading">Reset Password</h1>
-              </div>
-              <div className="right_bottom pt-3">
-                <form method="post" onSubmit={this.formSubmit}>
-                  <div className="change">
-                    <div className="input_icons">
-                      <i class="fa fa-user-o" aria-hidden="true"></i>
-                    </div>
-                    <input
-                      type="email"
-                      name="email"
-                      placeholder="Email"
-                      className="effect-8"
-                      value={this.state.email}
-                      onChange={this.handleChange}
-                      onBlur={this.validate} />
-                    <span className="focus-border">
-                      <i></i>
-                    </span>
+          <ConnectedHeader />
+          <div className="signup_right mx-auto pb-5 my-5 rounded">
+            <div className="right_top">
+              <h1 className="heading">Recupero password</h1>
+            </div>
+            <div className="right_bottom">
+              <form method="post" onSubmit={this.formSubmit}>
+                <div className="change">
+                  <div className="input_icons">
+                    <i className="fa fa-lock" aria-hidden="true"></i>
                   </div>
-                  <div className="error_div">
-                    {this.state.errors.email
-                      ? <p className="errmsg">{this.state.errors.email}</p>
-                      : ''
-                    }
-                  </div>
-                  <button className="login_btn pt-3">Submit</button>
-                </form>
-                <div className="final_error">
-                  {this.state.errors.invalid
-                    ? <p className="errmsg">{this.state.errors.invalid}</p>
-                    : ''
-                  }
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    onBlur={this.validate}
+                    value={this.state.password}
+                    onChange={this.handleChange}
+                    className="effect-8"
+                  />
+                  <span className="focus-border">
+                    <i></i>
+                  </span>
                 </div>
+                <div className="error_div">
+                  {this.state.errors.password ? (
+                    <p className="errmsg">{this.state.errors.password}</p>
+                  ) : (
+                    ""
+                  )}
+                </div>
+                <div className="change">
+                  <div className="input_icons">
+                    <i className="fa fa-lock" aria-hidden="true"></i>
+                  </div>
+                  <input
+                    type="password"
+                    name="confermaPassword"
+                    placeholder="Conferma password"
+                    onBlur={this.validate}
+                    value={this.state.confermaPassword}
+                    onChange={this.handleChange}
+                    className="effect-8"
+                  />
+                  <span className="focus-border">
+                    <i></i>
+                  </span>
+                </div>
+                <div className="error_div">
+                  {this.state.errors.confermaPassword ? (
+                    <p className="errmsg">{this.state.errors.confermaPassword}</p>
+                  ) : (
+                    ""
+                  )}
+                </div>
+                <button className="login_btn">Recupera</button>
+              </form>
+              <div className="final_error">
+                {this.state.errors.invalid ? (
+                  <p className="errmsg">{this.state.errors.invalid}</p>
+                ) : (
+                  ""
+                )}
               </div>
             </div>
           </div>
-          {
-            this.state.loading ? <Loading />:''
-          }
-          <Footer/>
+          {this.state.loading ? <Loading /> : ""}
+          <Footer />
         </div>
-        
-      )
+      );
     }
-
   }
+}
 
 export default Recover2;
