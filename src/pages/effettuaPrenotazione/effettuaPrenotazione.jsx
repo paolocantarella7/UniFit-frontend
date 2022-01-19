f72918031299c04e922166f85221f32897f05bc1
GGFlutterdev <luigi.allocca12345@live.it>
2022-01-19 19:21:02 +0100

    [PRZ] Effettua prenotazione

--- src/pages/effettuaPrenotazione/effettuaPrenotazione.jsx
+++ src/pages/effettuaPrenotazione/effettuaPrenotazione.jsx
@@ -1,44 +1,61 @@
-import React, { useState } from 'react';
-import ConnectedHeader from '../../components/header/header';
-import Footer from '../../components/footer/footer'
-import { Redirect } from 'react-router-dom';
+import React, { useState } from "react";
+import ConnectedHeader from "../../components/header/header";
+import Footer from "../../components/footer/footer";
+import { Redirect } from "react-router-dom";
 import Server from "../../config.json";
-import { DropdownButton, Dropdown , Button } from 'react-bootstrap';
-import Form from 'react-bootstrap/Form';
+import { DropdownButton, Dropdown, Button, Card } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
 
 class EffettuaPrenotazione extends React.Component {
   constructor() {
     super();
     this.state = {
       structures: [],
+      selectedFascia: "",
+      fasce: [],
       date: "",
-      slot: "", 
+      slot: "",
       loading: true,
-    }
+      selectedStructureId: "",
+    };
   }
 
   componentDidMount() {
-    this.struttureGet()
+    this.struttureGet();
   }
 
   struttureGet() {
-    var url = Server.API_URL + "admin/strutture/visualizzastrutture"
+    var url = Server.API_URL + "admin/strutture/visualizzastrutture";
     fetch(url)
-      .then(response => response.json())
-      .then(responseJson => {
+      .then((response) => response.json())
+      .then((responseJson) => {
         this.setState({ structures: responseJson.strutture }, () => {
-          this.setState({ loading: false })
-        })
+          this.setState({ loading: false });
+        });
       })
-      .catch(error => console.log(error))
+      .catch((error) => console.log(error));
+  }
+
+  fasceGet() {
+    var url =
+      Server.API_URL +
+      `prenotazione/getFasce?idStruttura=${this.state.selectedStructureId}`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({ fasce: responseJson.listaFasce }, () => {
+          this.setState({ loading: false });
+        });
+      })
+      .catch((error) => console.log(error));
   }
 
   goToPayment = () => {
     let errors = {};
-    if (this.state.date === "")
-      errors.date = "Inserisci una data"
-    if (this.state.slot === "")
-      errors.slot = "Inserisci una fascia"
+    if (this.state.selectedStructureId === "")
+      errors.selectedStructureId = "Inserisci una data";
+    if (this.state.selectedFascia === "")
+      errors.selectedFascia = "Inserisci una fascia";
     if (errors.date || errors.slot) {
       this.setState({ errors });
       return;
@@ -51,17 +68,20 @@ class EffettuaPrenotazione extends React.Component {
     });
   };
 
-  render() {
-    console.log(this.state.structures)
-    if (localStorage.getItem('isLogged') === 'false') {
+  handleChange = (event) => {
+    const {name, value} = event.target;
+    this.setState({[name]: value})
+  }
 
-      let user = localStorage.getItem('currentUser')
+  render() {
+    if (localStorage.getItem("isLogged") === "false") {
+      let user = localStorage.getItem("currentUser");
       user = JSON.parse(user);
 
       if (user.isAdmin) {
-        return <Redirect to="/adminArea" />
+        return <Redirect to="/adminArea" />;
       } else {
-        return <Redirect to="/home" />
+        return <Redirect to="/home" />;
       }
     } else {
       return (
@@ -70,23 +90,80 @@ class EffettuaPrenotazione extends React.Component {
           <div className="container-fluid text-dark rounded col-sm-10 col-10 text-center bg-white my-4 py-2">
             <h3 className="py-4 text-cyan">Effettua prenotazione</h3>
             <Form>
-              <DropdownButton id="dropdown-basic-button" title="Seleziona struttura">
-                {
-                  this.state.structures.map(struttura => (
-                    <Dropdown.Item href={struttura.id}>{struttura.nome}</Dropdown.Item>
-                  ))
-                }
-              </DropdownButton>
+              <div className="row my-4 mx-4">
+                <div className="col-6">
+                  <Card>
+                    <Card.Body>
+                      <Card.Text>{this.state.selectedStructureId}</Card.Text>
+                    </Card.Body>
+                  </Card>
+                </div>
+                <div className="col-6">
+                  {this.state.structures.size === 0 ? (
+                    ""
+                  ) : (
+                    <DropdownButton
+                      id="dropdown-basic-button"
+                      title="Seleziona struttura"
+                      onSelect={this.handleChange}
+                    >
+                      {this.state.structures.map((struttura) => (
+                        <Dropdown.Item eventKey={struttura.idStruttura}>
+                          {struttura.nome}
+                        </Dropdown.Item>
+                      ))}
+                    </DropdownButton>
+                  )}
+                </div>
+              </div>
 
+              {this.state.selectedStructureId === "" ? (
+                ""
+              ) : (
+                <div className="row my-4 mx-4">
+                  <div className="col-6">
+                    <Card>
+                      <Card.Body>
+                        <Card.Text>{this.state.selectedFascia}</Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </div>
+                  <div className="col-6">
+                    <DropdownButton
+                      id="dropdown-basic-button"
+                      title="Seleziona fascia"
+                      onSelect={this.handleChange}
+                    >
+                      {this.state.fasce.map((fascia) => (
+                        <Dropdown.Item eventKey={fascia}>
+                          {fascia}
+                        </Dropdown.Item>
+                      ))}
+                    </DropdownButton>
+                  </div>
+                </div>
+              )}
 
-
+              <div className="row">
+                <div className="col">
+                  <Card>
+                    <Card.Body>
+                      <Card.Text>{this.state.date}</Card.Text>
+                    </Card.Body>
+                  </Card>
+                </div>
+                <div className="col">
+                  <input type="date" name="date" onChange={this.handleChange}/>
+                </div>
+              </div>
 
               <div className="row">
                 <Button
                   variant="primary"
                   size="lg"
                   className="my-3 col-8 mx-auto"
-                  onClick={this.goToPayment}>
+                  onClick={this.goToPayment}
+                >
                   Paga ora
                 </Button>
               </div>
@@ -94,9 +171,8 @@ class EffettuaPrenotazione extends React.Component {
           </div>
           <Footer {...this.props} />
         </div>
-      )
+      );
     }
-
   }
 }
 
