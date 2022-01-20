9e823a1798fe623e5b13fa03eba8b0063401a822
GGFlutterdev <luigi.allocca12345@live.it>
2022-01-20 13:26:46 +0100

    [PRZ] Pagamento migliorato

--- src/pages/pagamento/pagamento.jsx
+++ src/pages/pagamento/pagamento.jsx
@@ -3,11 +3,9 @@ import Cards from "react-credit-cards";
 import "react-credit-cards/es/styles-compiled.css";
 import Footer from "../../components/footer/footer";
 import ConnectedHeader from "../../components/header/header";
-import { Link } from "react-router-dom";
 import { Button } from "react-bootstrap";
 import { toast } from "react-toastify";
 import Server from "../../config.json";
-import Datepicker from "react-multi-date-picker";
 
 export default class Pagamento extends React.Component {
   state = {
@@ -16,6 +14,7 @@ export default class Pagamento extends React.Component {
     focus: "",
     name: "",
     number: "",
+    expiryOnCard: "",
     errors: {},
   };
 
@@ -24,8 +23,20 @@ export default class Pagamento extends React.Component {
   };
 
   handleInputChange = (e) => {
+    
     const { name, value } = e.target;
-    this.setState({ [name]: value });
+    
+    this.setState({ [name] : value }, ()=>{
+      if(name === 'expiry'){
+        this.setState({
+          expiryOnCard: `${this.state.expiry.substring(this.state.expiry.indexOf("-")+1,this.state.expiry.length)}${this.state.expiry.substring(2,4)}`
+        })
+      }
+    });
+
+    
+
+    
   };
 
   formSubmit = (e) => {
@@ -33,7 +44,7 @@ export default class Pagamento extends React.Component {
     let errors = {};
     if (this.state.cvc.length != 3) errors.cvc = "CVC non valido valido";
     if (this.state.cvc.length === 0) errors.cvc = "Inserire CVC";
-    if (this.state.expiry.length != 4)
+    if (this.state.expiry.length === 0)
       errors.expiry = "Inserire una scadenza valida";
     if (this.state.name.length <= 0) errors.name = "Inserire l'intestatario";
     if (this.state.number.length != 16)
@@ -42,8 +53,7 @@ export default class Pagamento extends React.Component {
     if (errors.cvc || errors.expiry || errors.name || errors.number) {
       this.setState({ errors });
       return;
-    } else if (this.props.location.state.prenotazione === false){
-
+    } else if (this.props.location.state.prenotazione === false) {
       let user = localStorage.getItem("currentUser");
       user = JSON.parse(user);
 
@@ -140,7 +150,7 @@ export default class Pagamento extends React.Component {
         errors.cvc = value.length != 3 ? "Inserire un cvc valido" : "";
         break;
       case "expiry":
-        errors.expiry = value.length != 4 ? "Inserire una scadenza valida" : "";
+        errors.expiry = value.length === 0 ? "Inserire una scadenza valida" : "";
         break;
       default:
         break;
@@ -158,7 +168,7 @@ export default class Pagamento extends React.Component {
             <div className="col my-4">
               <Cards
                 cvc={this.state.cvc}
-                expiry={this.state.expiry}
+                expiry={this.state.expiryOnCard}
                 focused={this.state.focus}
                 name={this.state.name}
                 number={this.state.number}
@@ -199,24 +209,20 @@ export default class Pagamento extends React.Component {
                   )}
                 </div>
                 <div className="row">
-                  <div className="col-6">
-                  <input
-                    type="number"
-                    name="expiry"
-                    onBlur={this.validate}
-                    className="form-control my-2"
-                    placeholder="Expiry"
-                    onChange={this.handleInputChange}
-                    onFocus={this.handleInputFocus}
-                />
-                  </div>
-                  <div className="error_div">
+                  <div className="col-5">
+                    <input
+                      type="month"
+                      name="expiry"
+                      onChange={this.handleInputChange}
+                    />
+                    <div className="error_div">
                       {this.state.errors.expiry ? (
                         <p className="errmsg">{this.state.errors.expiry}</p>
                       ) : (
                         ""
                       )}
                     </div>
+                  </div>
                   <div className="col-6">
                     <input
                       type="number"
@@ -239,32 +245,32 @@ export default class Pagamento extends React.Component {
               </form>
             </div>
           </div>
-          {
-            this.props.location.state.prenotazione === false ? (
-              <div className="row ">
-            <Button
-              onClick={this.formSubmit}
-              variant="primary"
-              size="lg"
-              className="my-3 mx-4 mx-auto"
-            >
-              Paga ora {this.props.location.state.type === "Studente interno" ? "12€": "25€"}
-            </Button>
-          </div>
-            ) : (
-              <div className="row ">
-            <Button
-              onClick={this.formSubmit}
-              variant="primary"
-              size="lg"
-              className="my-3 mx-4 mx-auto"
-            >
-              Paga ora 2€
-            </Button>
-          </div>
-            )
-          }
-          
+          {this.props.location.state.prenotazione === false ? (
+            <div className="row ">
+              <Button
+                onClick={this.formSubmit}
+                variant="primary"
+                size="lg"
+                className="my-3 mx-4 mx-auto"
+              >
+                Paga ora{" "}
+                {this.props.location.state.type === "Studente interno"
+                  ? "12€"
+                  : "25€"}
+              </Button>
+            </div>
+          ) : (
+            <div className="row ">
+              <Button
+                onClick={this.formSubmit}
+                variant="primary"
+                size="lg"
+                className="my-3 mx-4 mx-auto"
+              >
+                Paga ora 2€
+              </Button>
+            </div>
+          )}
         </div>
         <Footer />
       </div>
