2dc5a6cbb2c4608c9b94f0d13409ff8c961b6377
Luke3199 <b.luke3199@gmail.com>
2022-01-21 21:57:37 +0100

    [ADM] Aggiunta cancellazione prenotazione

--- src/components/cardPrenotazioneUtente/cardPrenotazioneUtente.jsx
+++ src/components/cardPrenotazioneUtente/cardPrenotazioneUtente.jsx
@@ -9,23 +9,23 @@ class CardPrenotazioneUtente extends React.Component {
 
   render() {
     return (
-        <Card className="h-100 w-100 d-inline-block " id={this.props.prenotazione.idPrenotazione} style={{ margin: "10px 0px" }}>
-          <Card.Body>
-            <div className="row justify-content-between">
-              <div className="d-flex align-items-start mx-auto">
-                <Card.Title className="text-dark align-self-center">{this.props.prenotazione.strutturaPrenotata.nome} {this.props.prenotazione.dataPrenotazione}</Card.Title>
-              </div>
-              <div className="col-md-4 ">
-                <Link to="/showStructures" className="navbar-brand">
-                    <img className="img-responsive" src="assets/images/delete.png" alt="delete" data-toggle="modal" data-target="#modalElimina" onClick={() => this.props.onPress(this.props.struttura.idStruttura)} style={{ height: "25px", width: "25px" }} />
-                </Link>
-                <Link to={`/editReservation/${this.props.prenotazione.idPrenotazione}`} className="navbar-brand">
-                    <img className="img-responsive" src="assets/images/edit.png" alt="modify" style={{ height: "25px", width: "25px" }} />
-                </Link>
-              </div>
+      <Card className="h-100 w-100 d-inline-block " id={this.props.prenotazione.idPrenotazione} style={{ margin: "10px 0px" }}>
+        <Card.Body>
+          <div className="row justify-content-between">
+            <div className="d-flex align-items-start mx-auto">
+              <Card.Title className="text-dark align-self-center">{this.props.prenotazione.strutturaPrenotata.nome} {this.props.prenotazione.dataPrenotazione}</Card.Title>
             </div>
-          </Card.Body>
-        </Card>
+            <div className="col-md-4 ">
+              <Link to="/viewReservations" className="navbar-brand">
+                <img className="img-responsive" src="assets/images/delete.png" alt="delete" data-toggle="modal" data-target="#modalEliminaPrenotazione" onClick={() => this.props.onPress(this.props.prenotazione.idPrenotazione)} style={{ height: "25px", width: "25px" }} />
+              </Link>
+              <Link to={`/editReservation/${this.props.prenotazione.idPrenotazione}`} className="navbar-brand">
+                <img className="img-responsive" src="assets/images/edit.png" alt="modify" style={{ height: "25px", width: "25px" }} />
+              </Link>
+            </div>
+          </div>
+        </Card.Body>
+      </Card>
     );
   }
 }
--- src/pages/miePrenotazioni/miePrenotazioni.jsx
+++ src/pages/miePrenotazioni/miePrenotazioni.jsx
@@ -5,11 +5,12 @@ import { Redirect } from "react-router-dom";
 import Server from "../../config.json";
 import CardPrenotazioneUtente from "../../components/cardPrenotazioneUtente/cardPrenotazioneUtente";
 
-// Login page
+//  View Reservation page
 class VisualizzaPrenotazioni extends React.Component {
-    state = {
-        prenotazioni: []
-    };
+  state = {
+    prenotazioni: [],
+    idToDelete: -1,
+  };
 
   componentDidMount() {
     this.prenotazioniGet()
@@ -18,41 +19,89 @@ class VisualizzaPrenotazioni extends React.Component {
   prenotazioniGet() {
     let user = localStorage.getItem('currentUser')
     user = JSON.parse(user);
-    var url = Server.API_URL + 'prenotazione/prenotazioniUtente?idUtente='+user.idUtente
+    var url = Server.API_URL + 'prenotazione/prenotazioniUtente?idUtente=' + user.idUtente
     fetch(url)
       .then(response => response.json())
       .then(responseJson => {
-        this.setState({ prenotazioni: responseJson.prenotazioni })
+        this.setState({ prenotazioni: responseJson.prenotazioni }, () =>
+          this.setState({ loading: false }))
       })
       .catch(error => console.log(error))
   }
 
+  handleDelete = () => {
+    this.setState({ loading: true })
+
+    let user = localStorage.getItem("currentUser");
+    user = JSON.parse(user);
+
+    var data = new FormData()
+    data.append('idPrenotazione', this.state.idToDelete)
+    data.append('idUtente', user.idUtente)
+
+    this.setState({ loading: true })
+
+    var url = Server.API_URL + `prenotazione/cancellaPrenotazione`
+    fetch(url, {
+      method: 'POST',
+      body: data
+    }).then(response => response.json()
+    ).then(responseJson => {
+      if (responseJson.code === 200) {
+
+        this.prenotazioniGet();
+
+        window.location.reload(false);
+      }
+    })
+  }
+
   render() {
-    if (localStorage.getItem("isLogged") === "false") {
+    if (localStorage.getItem("isLogged") === false) {
+
+      return <Redirect to="/" />;
+    }
+    else {
       let user = localStorage.getItem("currentUser");
       user = JSON.parse(user);
 
-      if (user.isAdmin) {
+      if (user.isAdmin === 1) {
         return <Redirect to="/adminArea" />;
       } else {
-        return <Redirect to="/home" />;
+        return (
+          <>
+            <div>
+              <ConnectedHeader {...this.props} />
+              <div className="container-fluid text-dark rounded col-sm-10 col-10 text-center bg-white my-4">
+                <h3 className="py-4 text-cyan">Visualizza prenotazioni</h3>
+                {
+                  this.state.prenotazioni.map(prenotazione =>
+                    <CardPrenotazioneUtente
+                      key={prenotazione.idPrenotazione}
+                      prenotazione={prenotazione}
+                      onPress={(id) => this.setState({ idToDelete: id })} />)
+                }
+              </div>
+              <Footer {...this.props} />
+            </div>
+
+            <div className="modal" id="modalEliminaPrenotazione" tabIndex="-1" role="dialog">
+              <div className="modal-dialog" role="document">
+                <div className="modal-content">
+                  <div className="modal-body">
+                    <p>Sicuro di cancellare la prenotazione?</p>
+                  </div>
+                  <div className="modal-footer">
+                    <button type="button" className="btn btn-danger" data-dismiss="modal" onClick={() => this.handleDelete()}>Cancella</button>
+                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Annulla</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </>
+        );
       }
-    } else {
-      return (
-        <div>
-          <ConnectedHeader {...this.props} />
-          <div className="container-fluid text-dark rounded col-sm-10 col-10 text-center bg-white my-4">
-            <h3 className="py-4 text-cyan">Visualizza prenotazioni</h3>
-            {
-                this.state.prenotazioni.map(prenotazione => 
-                <CardPrenotazioneUtente key={prenotazione.idPrenotazione} prenotazione={prenotazione}/>)
-            }
-          </div>
-          <Footer {...this.props} />
-        </div>
-      );
     }
   }
 }
-
 export default VisualizzaPrenotazioni;
