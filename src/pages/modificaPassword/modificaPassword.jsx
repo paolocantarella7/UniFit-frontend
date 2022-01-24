2a4a8d733a69b9d3b605de394b5a219ba46fb904
Luke3199 <b.luke3199@gmail.com>
2022-01-24 13:09:05 +0100

    [TEST] Controllo accesso su pagina modifica password

--- src/pages/modificaPassword/modificaPassword.jsx
+++ src/pages/modificaPassword/modificaPassword.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Footer from "../../components/footer/footer";
 import ConnectedHeader from "../../components/header/header";
 import Server from "../../config.json";
+import { Redirect } from 'react-router-dom'
 import { toast } from "react-toastify";
 class ModificaPassword extends React.Component {
   constructor() {
@@ -76,92 +77,96 @@ class ModificaPassword extends React.Component {
   };
 
   render() {
-    return (
-      <div className="page">
-        <ConnectedHeader {...this.props} />
-        <form className="container-fluid text-dark rounded col-10 col-md-6 text-center bg-white my-4 py-4">
-          <h1 className="pt-4 text-cyan">Modifica password</h1>
+    if (localStorage.getItem("isLogged") === "false") {
+      return <Redirect to="/" />;
+    } else {
+      return (
+        <div className="page">
+          <ConnectedHeader {...this.props} />
+          <form className="container-fluid text-dark rounded col-10 col-md-6 text-center bg-white my-4 py-4">
+            <h1 className="pt-4 text-cyan">Modifica password</h1>
 
-          <div class="form-group py-2 col-8 mx-auto">
-            <input
-              name="password"
-              type="password"
-              class="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-              placeholder="Inserisci la nuova password..."
-              value={this.state.password}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div class="form-group py-2 col-8 mx-auto">
-            <input
-              name="confirmPassword"
-              type="password"
-              class="form-control"
-              id="exampleInputPassword1"
-              placeholder="Conferma la nuova password..."
-              value={this.state.confirmPassword}
-              onChange={this.handleChange}
-            />
-          </div>
-          <button
-            type="button"
-            class="btn btn-primary bg-cyan border col-6 rounded"
-            data-toggle="modal"
-            data-target="#exampleModal"
-          >
-            SALVA
-          </button>
-          <div
-            class="modal fade"
-            id="exampleModal"
-            tabindex="-1"
-            role="dialog"
-            aria-labelledby="exampleModalLabel"
-            aria-hidden="true"
-          >
-            <div class="modal-dialog" role="document">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h5 class="modal-title" id="exampleModalLabel">
-                    Avvertenza
-                  </h5>
-                  <button
-                    type="button"
-                    class="close"
-                    data-dismiss="modal"
-                    aria-label="Annulla"
-                  >
-                    <span aria-hidden="true">&times;</span>
-                  </button>
-                </div>
-                <div class="modal-body">
-                  Sei sicuro di voler modificare la tua password?
-                </div>
-                <div class="modal-footer">
-                  <button
-                    type="button"
-                    class="btn btn-secondary"
-                    data-dismiss="modal"
-                  >
-                    Annulla
-                  </button>
-                  <button
-                    onClick={this.formSubmit}
-                    type="button"
-                    class="btn btn-primary"
-                  >
-                    Conferma
-                  </button>
+            <div class="form-group py-2 col-8 mx-auto">
+              <input
+                name="password"
+                type="password"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="Inserisci la nuova password..."
+                value={this.state.password}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div class="form-group py-2 col-8 mx-auto">
+              <input
+                name="confirmPassword"
+                type="password"
+                class="form-control"
+                id="exampleInputPassword1"
+                placeholder="Conferma la nuova password..."
+                value={this.state.confirmPassword}
+                onChange={this.handleChange}
+              />
+            </div>
+            <button
+              type="button"
+              class="btn btn-primary bg-cyan border col-6 rounded"
+              data-toggle="modal"
+              data-target="#exampleModal"
+            >
+              SALVA
+            </button>
+            <div
+              class="modal fade"
+              id="exampleModal"
+              tabindex="-1"
+              role="dialog"
+              aria-labelledby="exampleModalLabel"
+              aria-hidden="true"
+            >
+              <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">
+                      Avvertenza
+                    </h5>
+                    <button
+                      type="button"
+                      class="close"
+                      data-dismiss="modal"
+                      aria-label="Annulla"
+                    >
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">
+                    Sei sicuro di voler modificare la tua password?
+                  </div>
+                  <div class="modal-footer">
+                    <button
+                      type="button"
+                      class="btn btn-secondary"
+                      data-dismiss="modal"
+                    >
+                      Annulla
+                    </button>
+                    <button
+                      onClick={this.formSubmit}
+                      type="button"
+                      class="btn btn-primary"
+                    >
+                      Conferma
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </form>
-        <Footer {...this.props} />
-      </div>
-    );
+          </form>
+          <Footer {...this.props} />
+        </div>
+      );
+    }
   }
 }
 export default ModificaPassword;
