ac61f240e0e30c85f7461c45ba9172937aa60e01
Luke3199 <b.luke3199@gmail.com>
2022-01-24 13:07:34 +0100

    [PRU] Controlli accesso PRU

--- src/pages/areaPersonaleUtente/areaPersonaleUtente.jsx
+++ src/pages/areaPersonaleUtente/areaPersonaleUtente.jsx
@@ -1,64 +1,74 @@
 import React from "react";
 import Footer from "../../components/footer/footer";
 import ConnectedHeader from "../../components/header/header";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import "./areaPersonaleUtente.scss";
 import AreaPersonaleUtenteSvg from "../../areaPersonaleUtente.svg";
 
 class AreaPersonaleUtente extends React.Component {
   render() {
-    return (
-      <div className="page">
-        <ConnectedHeader {...this.props} />
-        <div className="container-fluid text-dark rounded w-75 text-center bg-white my-4">
-          <h1 className="pt-4">Area personale utente</h1>
-          <div className="row py-3 px-3">
-            <button
-              type="button"
-              className="btn btn-primary btn-lg mx-auto bg-cyan border h3 col-xs-12 col-md-8"
-            >
-              Le mie prenotazioni
-            </button>
+    if (localStorage.getItem("isLogged") === "false") {
+      return <Redirect to="/" />;
+    } else {
+      let user = localStorage.getItem("currentUser");
+      user = JSON.parse(user);
+
+      if (user.isAdmin === 1) {
+        return <Redirect to="/adminArea" />;
+      } else {
+        return (
+          <div className="page">
+            <ConnectedHeader {...this.props} />
+            <div className="container-fluid text-dark rounded w-75 text-center bg-white my-4">
+              <h1 className="pt-4">Area personale utente</h1>
+              <div className="row py-3 px-3">
+                <button
+                  type="button"
+                  className="btn btn-primary btn-lg mx-auto bg-cyan border h3 col-xs-12 col-md-8"
+                >
+                  Le mie prenotazioni
+                </button>
+              </div>
+              <Link to={"/editPassword"} className={`nav-link`}>
+                <div className="row py-3 px-3">
+                  <button
+                    type="button"
+                    className="btn btn-primary btn-lg mx-auto bg-cyan border col-xs-12 col-md-8"
+                  >
+                    Modifica password
+                  </button>
+                </div>
+              </Link>
+              <Link to={"/userDetails"} className={`nav-link`}>
+                <div className="row py-3 px-3">
+                  <button
+                    type="button"
+                    className="btn btn-primary btn-lg mx-auto bg-cyan border col-xs-12 col-md-8"
+                  >
+                    Visualizza informazioni personali
+                  </button>
+                </div>
+              </Link>
+              <Link to={"/deleteAccount"} className={`nav-link`}>
+                <div className="row py-3 px-3">
+                  <button
+                    type="button"
+                    className="btn btn-primary btn-lg mx-auto bg-cyan border col-xs-12 col-md-8"
+                  >
+                    Cancella account
+                  </button>
+                </div>
+              </Link>
+
+              <img className="pt-3 pb-4 img-fluid mx-auto" width={180} src={AreaPersonaleUtenteSvg} alt="React Logo" />
+
+            </div>
+
+            <Footer {...this.props} />
           </div>
-          <Link to={"/editPassword"} className={`nav-link`}>
-            <div className="row py-3 px-3">
-              <button
-                type="button"
-                className="btn btn-primary btn-lg mx-auto bg-cyan border col-xs-12 col-md-8"
-              >
-                Modifica password
-              </button>
-            </div>
-          </Link>
-          <Link to={"/userDetails"} className={`nav-link`}>
-            <div className="row py-3 px-3">
-              <button
-                type="button"
-                className="btn btn-primary btn-lg mx-auto bg-cyan border col-xs-12 col-md-8"
-              >
-                Visualizza informazioni personali
-              </button>
-            </div>
-          </Link>
-          <Link to={"/deleteAccount"} className={`nav-link`}>
-            <div className="row py-3 px-3">
-              <button
-                type="button"
-                className="btn btn-primary btn-lg mx-auto bg-cyan border col-xs-12 col-md-8"
-              >
-                Cancella account
-              </button>
-            </div>
-          </Link>
-
-          <img className="pt-3 pb-4 img-fluid mx-auto" width={180} src={AreaPersonaleUtenteSvg} alt="React Logo" />
-
-        </div>
-
-        <Footer {...this.props} />
-      </div>
-    );
+        );
+      }
+    }
   }
 }
-
 export default AreaPersonaleUtente;
--- src/pages/informazioniPersonaliUtente/informazioniPersonaliUtente.jsx
+++ src/pages/informazioniPersonaliUtente/informazioniPersonaliUtente.jsx
@@ -3,20 +3,28 @@ import Footer from '../../components/footer/footer'
 import ConnectedHeader from '../../components/header/header'
 import './informazioniPersonaliUtente.scss';
 import InformazioniPersonaliSvg from "../../informazioniPersonali.svg";
+import { Redirect } from 'react-router-dom'
 
 class InformazioniPersonaliUtente extends React.Component {
   render() {
-    let user = localStorage.getItem("currentUser");
-    user = JSON.parse(user);
-    return (
-      <div className="page">
-          <ConnectedHeader {...this.props}/>
-          <div className="w-75 bg-white mx-auto pb-5 my-5 rounded">
+    if (localStorage.getItem("isLogged") === "false") {
+      return <Redirect to="/" />;
+    } else {
+      let user = localStorage.getItem("currentUser");
+      user = JSON.parse(user);
+
+      if (user.isAdmin === 1) {
+        return <Redirect to="/adminArea" />;
+      } else {
+        return (
+          <div className="page">
+            <ConnectedHeader {...this.props} />
+            <div className="w-75 bg-white mx-auto pb-5 my-5 rounded">
               <div className="row">
                 <h1 className="mx-auto pt-4 text-cyan text-center px-4">Info Su {user.nome} {user.cognome}</h1>
               </div>
               <div className="container  my-4">
-               
+
                 <div className="row">
                   <h4 className="mx-auto text-cyan">Email</h4>
                 </div>
@@ -53,16 +61,17 @@ class InformazioniPersonaliUtente extends React.Component {
                 <div className="row">
                   <p className="mx-auto text-black">{user.nazionalita}</p>
                 </div>
-              <div className="row">
-              <img className="pt-5 img-fluid mx-auto" width={180} src={InformazioniPersonaliSvg} alt="React Logo" />
-              </div>
+                <div className="row">
+                  <img className="pt-5 img-fluid mx-auto" width={180} src={InformazioniPersonaliSvg} alt="React Logo" />
+                </div>
               </div>
             </div>
 
-          <Footer />
-      </div>
-    );
+            <Footer />
+          </div>
+        );
+      }
+    }
   }
 }
-
 export default InformazioniPersonaliUtente;
