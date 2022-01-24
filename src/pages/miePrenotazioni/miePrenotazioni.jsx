b8222762aa58fce1f33a06206b41c20a8c5d4c05
Luke3199 <b.luke3199@gmail.com>
2022-01-24 18:46:23 +0100

    [TEST] Fix classname error

--- src/pages/miePrenotazioni/miePrenotazioni.jsx
+++ src/pages/miePrenotazioni/miePrenotazioni.jsx
@@ -39,6 +39,7 @@ class VisualizzaPrenotazioni extends React.Component {
 
   handleDelete = () => {
     this.setState({ loading: true });
+    
     let user = localStorage.getItem("currentUser");
     user = JSON.parse(user);
 
@@ -53,16 +54,20 @@ class VisualizzaPrenotazioni extends React.Component {
     })
       .then((response) => response.json())
       .then((responseJson) => {
-        console.log(responseJson)
         if (responseJson.code === 200) {
           this.prenotazioniGet();
 
+          this.setState({ loading: false });
+
           window.location.reload(false);
-        } else if (responseJson.code === 400){
-          toast.error(responseJson.msg , {
-            autoClose : 5000,
-            className: "errorToast"
-          }) 
+        } else if (responseJson.code === 400) {
+          responseJson.error.map(error => {
+            toast.error(error.msg, {
+              autoClose: 5000,
+              className: "errorToast"
+            })
+          })
+          this.setState({ loading: false });
         }
       });
   };
@@ -84,20 +89,20 @@ class VisualizzaPrenotazioni extends React.Component {
               <div className="container-fluid text-dark rounded col-sm-10 col-10 text-center bg-white my-4">
                 <h1 className="py-4 text-cyan">Le mie prenotazioni</h1>
                 {this.state.prenotazioni.length === 0 &&
-                   <div className="mb-4">
-                   <img className="my-5" width="200" src={NessunaPrenotazioneSvg} alt="Nessuna Prenotazione" />
-                   <p className="pb-4">Non ci sono prenotazioni</p>
+                  <div className="mb-4">
+                    <img className="my-5" width="200" src={NessunaPrenotazioneSvg} alt="Nessuna Prenotazione" />
+                    <p className="pb-4">Non ci sono prenotazioni</p>
 
-                   <Link to={`/makeReservation`} className={`nav-link`}>
-            <div className="row py-3 px-3">
-              <button
-                type="button"
-                className="btn btn-primary btn-lg mx-auto bg-cyan border col-10 col-sm-7 col-md-5 mb-2">
-                Prenota una struttura
-              </button>
-            </div>
-          </Link>
-                 </div>
+                    <Link to={`/makeReservation`} className={`nav-link`}>
+                      <div className="row py-3 px-3">
+                        <button
+                          type="button"
+                          className="btn btn-primary btn-lg mx-auto bg-cyan border col-10 col-sm-7 col-md-5 mb-2">
+                          Prenota una struttura
+                        </button>
+                      </div>
+                    </Link>
+                  </div>
                 }
 
                 {this.state.prenotazioni.map((prenotazione) => (
