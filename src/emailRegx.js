10a3a14213b7a8ccab274ed7b98c445d32ee913c
GGFlutterdev <luigi.allocca12345@live.it>
2022-01-24 18:24:38 +0100

    [TEST] Email regex

--- src/emailRegx.js
+++ src/emailRegx.js
@@ -1,2 +1,2 @@
-const validEmailRegex = RegExp(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+const validEmailRegex = RegExp(/^[a-zA-Z0-9.-]+[@][a-zA-Z0-9.-]+[.][a-zA-Z]{2,6}/);
 export default  validEmailRegex;
